caa4b9de80655c2422c7516420a71dafe0910842
root <root@snuc.lan>
2025-05-13 21:17:49 +0800

    Add lines visualization for the people tracking wrt local robot frame

--- src/relative_track_transformer.cpp
+++ src/relative_track_transformer.cpp
@@ -7,6 +7,8 @@
 #include <hdl_people_tracking/Cluster.h>
 #include <hdl_people_tracking/ExtendedTrackArray.h>
 #include <hdl_people_tracking/ExtendedTrack.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <boost/array.hpp>
@@ -21,11 +23,13 @@ public:
         pnh.param<std::string>("tracks_topic", tracks_topic_, "/hdl_people_tracking_nodelet/tracks");
         pnh.param<std::string>("odom_topic", odom_topic_, "/odom");
         pnh.param<std::string>("output_topic", output_topic_, "/hdl_people_tracking_nodelet/tracks_relative");
+        pnh.param<std::string>("marker_topic", marker_topic_, "/track_lines");
 
         tracks_sub_ = nh.subscribe(tracks_topic_, 10, &RelativeTrackTransformer::tracksCallback, this);
         odom_sub_   = nh.subscribe(odom_topic_, 10, &RelativeTrackTransformer::odomCallback, this);
 
         tracks_pub_ = nh.advertise<hdl_people_tracking::ExtendedTrackArray>(output_topic_, 10);
+        marker_pub_ = nh.advertise<visualization_msgs::MarkerArray>(marker_topic_, 10);
 
         odom_received_ = false;
     }
@@ -34,10 +38,12 @@ private:
     ros::Subscriber tracks_sub_;
     ros::Subscriber odom_sub_;
     ros::Publisher  tracks_pub_;
+    ros::Publisher  marker_pub_;
 
     std::string tracks_topic_;
     std::string odom_topic_;
     std::string output_topic_;
+    std::string marker_topic_;
 
     geometry_msgs::Point robot_position_;
     double robot_yaw_;
@@ -88,13 +94,17 @@ private:
         hdl_people_tracking::ExtendedTrackArray relative_tracks;
         relative_tracks.header = msg->header;
 
+        visualization_msgs::MarkerArray marker_array;
+        marker_array.markers.reserve(msg->tracks.size());
+
         double cos_yaw = std::cos(-robot_yaw_);
         double sin_yaw = std::sin(-robot_yaw_);
+        size_t idx = 0;
 
         for (const auto& track : msg->tracks) {
             hdl_people_tracking::ExtendedTrack relative;
 
-            // Transform and assign position
+            // Transform position
             double dx = track.pos.x - robot_position_.x;
             double dy = track.pos.y - robot_position_.y;
             double dz = track.pos.z - robot_position_.z;
@@ -102,7 +112,7 @@ private:
             relative.pos.y = dx * sin_yaw + dy * cos_yaw;
             relative.pos.z = dz;
 
-            // Transform and assign velocity
+            // Transform velocity
             double vx = track.vel.x;
             double vy = track.vel.y;
             double vz = track.vel.z;
@@ -115,17 +125,39 @@ private:
             relative.vel_cov = rotateCovariance(track.vel_cov, cos_yaw, sin_yaw);
 
             // Copy other fields
-            relative.id        = track.id;
-            relative.age       = track.age;
+            relative.id         = track.id;
+            relative.age        = track.age;
             relative.associated = track.associated;
 
-            // Compute and assign bearing
+            // Compute bearing
             relative.bearing = std::atan2(relative.pos.y, relative.pos.x);
-
             relative_tracks.tracks.push_back(relative);
+
+            // Create line marker with 1-second lifetime
+            visualization_msgs::Marker m;
+            m.header = msg->header;
+            m.header.frame_id = "livox_frame";
+            m.ns = "track_lines";
+            m.id = idx++;
+            m.type = visualization_msgs::Marker::LINE_LIST;
+            m.action = visualization_msgs::Marker::ADD;
+            m.scale.x = 0.02;
+            m.color.r = 1.0;
+            m.color.g = 0.0;
+            m.color.b = 0.0;
+            m.color.a = 1.0;
+            m.lifetime = ros::Duration(1.0);  // auto-expire
+
+            geometry_msgs::Point p0;
+            p0.x = 0; p0.y = 0; p0.z = 0;
+            m.points.push_back(p0);
+            m.points.push_back(relative.pos);
+            marker_array.markers.push_back(m);
         }
 
+        // Publish both
         tracks_pub_.publish(relative_tracks);
+        marker_pub_.publish(marker_array);
     }
 };
 
